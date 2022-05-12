23ac8b479147c497b9e76f6659854e2d37c5900f
Biraj Poudel <birajpoudel23@gmail.com>
2022-05-12 12:53:09 +0545

    Django admin UI|django-jazzmin

--- GymMgmtSys/settings.py
+++ GymMgmtSys/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -136,3 +137,17 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+JAZZMIN_UI_TWEAKS = {
+
+    "theme": "simplex",
+}
+
+JAZZMIN_SETTINGS= {
+    "site_title": "GMS Admin",
+    "site_header": "GMS Admin",
+    "order_with_respect_to": ["auth"],
+    "welcome_sign": "Welcome to the Arnold's GYM",
+    "copyright": "Arnold's Gym Ltd",
+
+}
